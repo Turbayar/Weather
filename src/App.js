7643ca2124d193d161c7eeadef14e4e57cbbec43
Turbayar <Tito@Bilguuns-iMac.local>
2022-03-15 14:11:49 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
+const axios = require("axios");
+
 function App() {
+  const [location, setLocation] = useState();
+  const [place, setPlace] = useState([]);
+
+  const getData = async() => {
+    const loc = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=pk.eyJ1IjoidHVyYmF5YXIiLCJhIjoiY2wwYWc4Mm96MDBxdDNkbWo5eWNjZ21vZiJ9.yQUAfjQWJdDuxktSX7AqAA`);
+    setPlace(loc.data.features)
+    console.log(place)
+  } 
+
+
+  const getName = (e) => {
+     setLocation(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' onChange={getName} ></input>
+      <button onClick={getData}> Click</button>
+      {
+        place.map(({place_name, center}, i)=>{
+          return <p key={i} > {place_name} == {center[1]}, {center[0]}</p>
+        })
+      }
     </div>
   );
 }
